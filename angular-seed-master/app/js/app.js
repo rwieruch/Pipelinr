39d2d7f96b8e9d909dae6cfc91b1e7f1d621ba55
Robin W <s8190561@mail.zih.tu-dresden.de>
2014-05-18 18:23:17 +0200

    client sided access rights for routes

--- angular-seed-master/app/js/app.js
+++ angular-seed-master/app/js/app.js
@@ -11,7 +11,7 @@ angular.module('myApp', [
   'ui.bootstrap'
 ]).
 config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1', access: { isFree: true } });
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.when('/register', {templateUrl: 'partials/register.html', controller: 'RegisterCtrl'});
   $routeProvider.otherwise({redirectTo: '/register'});
@@ -23,13 +23,7 @@ config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvide
   // register listener to watch route changes
   $rootScope.$on( "$routeChangeStart", function(event, next, current) {
     if (!Session.isLogged) {
-      // no logged user, we should be going to #login
-      if ( next.templateUrl == "partials/register.html" ) {
-        // already going to #login, no redirect needed
-      } else {
-        // not going to #login, we should redirect now
-        $location.path( "/view1" );
-      }
-    }         
+      $location.path( "/register" );
+    }      
   });
 });
